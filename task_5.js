3b1404b8d37ba92794dd220c5082566c28b3df84
sachgurjar <gurjarsachin34@gmail.com>
2022-02-06 21:58:13 -1200

    change 3

---
+++ task_5.js
@@ -0,0 +1,23 @@
+function fillstate()
+{
+    switch (Country.selectedIndex) {
+      case 1:
+        var ins = ("madhya pradesh", "utter pradesh");
+        filldd(ins);
+        break;
+
+      case 1:
+        var us = ("california", "arizona", "montana");
+        filldd(us);
+        break;
+      case 1:
+        var pak = ("karachi", "lahore");
+        filldd(pak);
+        break;
+    }
+}
+function removedd(dd){
+  
+    for()
+    
+}
