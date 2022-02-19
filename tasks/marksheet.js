509d833ca5b6d6479c03c877410ef11d589f0a2a
sach-gurjar <gurjarsachin34@gmail.com>
2022-02-18 12:27:43 -1200

    update 18-02-2022

--- tasks/marksheet.js
+++ tasks/marksheet.js
@@ -36,64 +36,89 @@ function call2() {
 }
 
 function call3() {
-     var hin = parseFloat(hindi.value)
-     var eng = parseFloat(english.value)
-     var mat = parseFloat(math.value)
-     var phy = parseFloat(physics.value)
-     var che = parseFloat(chemistry.value)
-     
-     var tm = hin + eng + mat + phy + che;
-     var pr = ((hin + eng + mat + phy + che) / 500) * 100;
+  var hin = parseFloat(hindi.value);
+  var eng = parseFloat(english.value);
+  var mat = parseFloat(math.value);
+  var phy = parseFloat(physics.value);
+  var che = parseFloat(chemistry.value);
 
-    //  if(hin>=0 && hin<33){
-    //      hinn="*"
-    //  }
-    //  else if(hin>=75 && hin<=100){
-    //      hinn="D"
-    //  }
-    //  if (eng >= 0 && eng < 33) {
-    //    engg = "*";
-    //  } else if (eng >= 75 && eng <= 100) {
-    //    engg = "D";
-    //  }
-    //  if (mat >= 0 && mat < 33) {
-    //    matt = "*";
-    //  } else if (mat >= 75 && mat <= 100) {
-    //    matt = "D";
-    //  }
-    //  if (phy >= 0 && phy < 33) {
-    //    phyy = "*";
-    //  } else if (phy >= 75 && phy <= 100) {
-    //    phyy = "D";
-    //  }
-    //  if (che >= 0 && che < 33) {
-    //    chee = "*";
-    //  } else if (che >= 75 && che <= 100) {
-    //    chee = "D";
-    //  }
+  var tm = hin + eng + mat + phy + che;
+  var pr = ((hin + eng + mat + phy + che) / 500) * 100;
 
-    if(pr>=60 && pr<=100){
-        dd= "first division"
+   if(hin>=0 && hin<=33){
+       hinn="*"
+   }
+    else if(hin>=34 && hin<=74){
+       hinn="pass"
     }
-    else if (pr >= 45 && pr <= 59) {
-      dd = "second division";
-    }
-    else if (pr >= 33 && pr <= 44) {
-      dd = "third division";
-    }
+   else if(hin>=75 && hin<=100){
+       hinn="D"
+   }
+   if (eng >= 0 && eng <=33) {
+     engg = "*";
+   } 
+   else if (eng >= 34 && eng <= 74) {
+     engg = "pass";
+   } 
+   else if (eng >= 75 && eng <= 100) {
+     engg = "D";
+   }
+   if (mat >= 0 && mat <=33) {
+     matt = "*";
+   }
+    else if (mat >=34 && mat <=74) {
+     matt = "pass";
+   }
+    else if (mat >= 75 && mat <= 100) {
+     matt = "D";
+   }
+   if (phy >= 0 && phy <=33) {
+     phyy = "*";
+   } 
+   else if (phy >= 34 && phy <= 74) {
+     phyy = "pass";
+   } 
+   else if (phy >= 75 && phy <= 100) {
+     phyy = "D";
+   }
+    if (che >= 0 && che <= 33) {
+     chee = "*";
+   } 
+   else if (che >= 34 && che <= 74) {
+     chee = "pass";
+   } 
+   else if (che >= 75 && che <= 100) {
+     chee = "D";
+   }
+
+  if (pr >= 60 && pr <= 100) {
+    dd = "first division";
+  } else if (pr >= 45 && pr <= 59) {
+    dd = "second division";
+  } else if (pr >= 33 && pr <= 44) {
+    dd = "third division";
+  }
 
   var o = "<table border='1' width='60%' cellspacing='0' cellpadding='5'>";
       o += "<th bgcolor='#ACA2A9' colspan ='6'>Marks</th>";
-      o +="<tr><th>Paper Code</th><th>Subject</th><th>Min marks</th><th>max marks</th><th>Marks Obtained</th><th>Remark</th></th></tr>"
-      o += "<tr><th>101.</th><th>Hindi</th><th>33</th><th>100</th><th>"+hindi.value+"</th></tr>"
-      o +="<tr><th>102.</th><th>English</th><th>33</th><th>100</th><th>"+english.value+"</th></tr>"
-      o +="<tr><th>103.</th><th>Math</th><th>33</th><th>100</th><th>"+math.value+"</th></tr>"
-      o +="<tr><th>104.</th><th>Physics</th><th>33</th><th>100</th><th>"+physics.value+"</th></tr>"
-      o +="<tr><th>105.</th><th>Chemistry</th><th>33</th><th>100</th><th>"+chemistry.value+"</th></tr>"
-      o += `<tr bgcolor='#9CAA82' ><th colspan ='2'><b>Total marks = ${tm} </b></th><th  colspan='2'><b>Percentage= ${pr} </b></th><th colspan='2'><b>Division= ${dd} </b></th></tr></table>`;
 
-     
+      o +="<tr><th>Paper Code</th><th>Subject</th><th>Min marks</th>"+
+           "<th>max marks</th><th>Marks Obtained</th><th>Remark</th></th></tr>";
+
+      o +="<tr><th>101.</th><th>Hindi</th><th>33</th><th>100</th><th>"+hindi.value +"</th></tr>";
+
+      o +="<tr><th>102.</th><th>English</th><th>33</th><th>100</th><th>"+english.value +"</th></tr>";
+
+      o +="<tr><th>103.</th><th>Math</th><th>33</th><th>100</th><th>" + math.value + "</th></tr>";
+    
+      o +="<tr><th>104.</th><th>Physics</th><th>33</th><th>100</th><th>" + physics.value + "</th></tr>";
+
+      o +="<tr><th>105.</th><th>Chemistry</th><th>33</th><th>100</th><th>" +chemistry.value +"</th></tr>";
+
+      o += `<tr bgcolor='#9CAA82' ><th colspan ='2'><b>Total marks = ${tm} </b></th>
+           <th  colspan='2'><b>Percentage= ${pr} </b></th><th colspan='2'><b>Division= ${dd} </b></th></tr></table>`;
+
   logoimg2.innerHTML = o;
 
-  alert(totalmarksid)
+  
 }
